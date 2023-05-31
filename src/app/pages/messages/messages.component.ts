fe47e98552d63b478c612d7e60dc5f6d292bb5c9
mi379 <mkzbdgfh@gmail.com>
2023-06-01 04:04:27 +0700

    a small change

--- src/app/pages/messages/messages.component.ts
+++ src/app/pages/messages/messages.component.ts
@@ -10,7 +10,7 @@ import { Component,OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
   state:State = window.history.state
-  profile:Profile = state.profile
+  profile:Profile = this.state.profile
 
   ngOnInit(){
   	
