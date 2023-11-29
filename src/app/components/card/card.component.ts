99c604e76621fcf0389cd271026abbe3ab3d0140
mkzbdfgh@gmail.com <mkzbdfgh@gmail.com>
2023-11-30 06:12:03 +0700

    test

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -10,6 +10,16 @@ import { Profile } from '../../ngrx/user/user.reducer'
 export class CardComponent {
   @Input() user: Compare | undefined
   @Input() message : Message | undefined
+  @Input() _id : string | undefined
+  
+  unreadCounter({unreadCounter,sender}:Message):boolean{
+    if(unreadCounter>0 && sender.usersRef !== this._id){
+      return true
+    }
+    else{
+      return false
+    }
+  }
 }
 
 type Compare = Profile & {
