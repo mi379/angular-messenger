2c0ab499f002969a27b65f6f0d22d8b28327862c
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-01 05:05:47 +0700

    Update magnifying-glass.component.ts

--- src/app/components/icons/magnifying-glass/magnifying-glass.component.ts
+++ src/app/components/icons/magnifying-glass/magnifying-glass.component.ts
@@ -1,1 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-magnifying-glass',
+  templateUrl: './magnifying.component.html',
+  styleUrls: ['./magnifying.component.css']
+})
+export class MagnifyingGlassComponent {
 
