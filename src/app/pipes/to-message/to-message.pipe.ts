26790ec984dcfc6af356f8021ee8abb789e538e5
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-07 09:02:32 +0700

    Update to-message.pipe.ts

--- src/app/pipes/to-message/to-message.pipe.ts
+++ src/app/pipes/to-message/to-message.pipe.ts
@@ -4,7 +4,7 @@ import { Message,IncomingMessage } from '../../pages/home/home.component'
 @Pipe({
   name: 'toMessage'
 })
-export class InputPipe implements PipeTransform {
+export class ToMessage implements PipeTransform {
 
   transform(message:IncomingMessage):Message{
     return message as Message
