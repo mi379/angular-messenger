112616167e579c49610a80d38f055266a73e4e49
mi379 <mi379>
2023-12-11 03:31:05 +0700

    update card.component.ts

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -25,7 +25,7 @@ export class CardComponent {
       var message:Message = this.message as Message
       var {unreadCounter,sender}:Message = message
 
-      return message?.unreadCounter > 0 && sender.usersRef === String(this._id) 
+      return message?.unreadCounter > 0 && sender.usersRef !== String(this._id) 
         ? true
         : false
     }
