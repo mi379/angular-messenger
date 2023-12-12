862b44b695dd8827c7aa70fbe163a4504cf6a901
mkzbdfgh@gmail.com <mkzbdfgh@gmail.com>
2023-12-13 04:41:29 +0700

    adding register link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component'
 import { SearchComponent } from './pages/search/search.component'
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -27,6 +28,10 @@ const routes:Routes = [
     path:'search', 
     component:SearchComponent, 
     canActivate:[AuthenticationGuard]
+  }, 
+  {
+    path:'register', 
+    component:RegisterComponent
   }, 
   {
     path:'**',
