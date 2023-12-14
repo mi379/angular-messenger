acc4b377958cd059fe26f38935a8a01b172f6d3f
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-14 13:08:32 +0700

    Update callback.component.ts

--- src/app/pages/callback/callback.component.ts
+++ src/app/pages/callback/callback.component.ts
@@ -1,1 +1,14 @@
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-callback',
+  templateUrl: './callback.component.html',
+  styleUrls: ['./callback.component.css']
+})
+
+export class CallbackComponent {
+
+
+}
+  
+                                                              }
