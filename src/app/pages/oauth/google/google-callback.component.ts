44b78ba94f099a0c11ecdb2c1e3e9019f2b37fff
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-16 23:53:01 +0700

    Update google-callback.component.ts

--- src/app/pages/oauth/google/google-callback.component.ts
+++ src/app/pages/oauth/google/google-callback.component.ts
@@ -1,1 +1,12 @@
-o
+
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'google-callback',
+  templateUrl: './google-callback.component.html',
+  styleUrls: ['./google-callback.component.css']
+})
+
+export class GoogleCallbackComponent{
+  
+}
