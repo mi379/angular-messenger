d6bcb2112397504bc2ba189e9690ca6e9aeb2cf1
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-17 14:35:12 +0700

    Update register.component.ts

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -29,7 +29,7 @@ export class RegisterComponent implements OnInit{
 
   ngOnInit(){
     this.oauthInfoChannel.onmessage = e => {
-      alert(JSON.stringify(e)) 
+      this.auth.login(true,e) 
     }
   }
   
