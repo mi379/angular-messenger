b683c00be329e0717437f1475cd461a7284ebf53
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-22 05:43:18 +0700

    Update register.component.ts

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -10,16 +10,26 @@ import { Reducers } from '../login/login.component'
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'], 
-  animations: [
-    trigger('fade', [
-      state('fadeOut', 
-      style({ opacity: 0 }), 
-      transition(
-        'fadeOut',
-        [animate(500)]
-      ),
-    ]),
-  ]
+   animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ])
 })
 
 export class RegisterComponent implements OnInit{
